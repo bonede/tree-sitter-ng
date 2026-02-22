5ff3f02d630ffa55948f040884f753c72bbd0882
David Baker Effendi <david@brokk.ai>
2026-02-22 13:47:05 +0200

    refactor: TSAutoCloseableTest cleanup and documentation
    Move internal comments to class-level Javadoc and remove empty test stubs.

--- tree-sitter/src/test/java/org/treesitter/TSAutoCloseableTest.java
+++ tree-sitter/src/test/java/org/treesitter/TSAutoCloseableTest.java
@@ -3,6 +3,16 @@ package org.treesitter;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+/**
+ * Tests for AutoCloseable implementation in Tree-sitter Java bindings.
+ * <p>
+ * Note on TSTree: TSTree is usually created by parser.parse().
+ * We test idempotency via manual instantiation if we can't easily get a valid tree without a language.
+ * TSTree constructor is package-private, so we test via parser if we had a language.
+ * Since we can't easily create a valid TSTree without a language in this generic test
+ * and calling methods on a NULL tree pointer crashes, we'll focus on the classes
+ * we can safely instantiate and close.
+ */
 class TSAutoCloseableTest {
 
     @Test
@@ -21,20 +31,6 @@ class TSAutoCloseableTest {
         assertDoesNotThrow(parser::close);
     }
 
-    @Test
-    void testTreeAutoClose() {
-        // TSTree is usually created by parser.parse(). 
-        // We test idempotency via manual instantiation if we can't easily get a valid tree without a language.
-        // TSTree constructor is package-private, so we test via parser if we had a language, 
-        // but for this test we can just ensure the parser doesn't crash on close.
-    }
-
-    @Test
-    void testTreeDoubleClose() {
-        // Since we can't easily create a valid TSTree without a language in this generic test
-        // and calling methods on a NULL tree pointer crashes, we'll focus on the classes
-        // we can safely instantiate and close.
-    }
 
     @Test
     void testQueryCursorAutoClose() {

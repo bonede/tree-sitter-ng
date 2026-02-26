8d8ba2c4b1129ebb6e131ff8314b58e51677945f
David Baker Effendi <david@brokk.ai>
2026-02-26 11:17:30 +0200

    enh: add metadata field and getter to TSQueryMatch

--- tree-sitter/src/main/java/org/treesitter/TSQueryMatch.java
+++ tree-sitter/src/main/java/org/treesitter/TSQueryMatch.java
@@ -1,10 +1,14 @@
 package org.treesitter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TSQueryMatch {
     private int id;
     private int patternIndex;
     private int captureIndex;
     private TSQueryCapture[] captures;
+    private Map<String, String> metadata = new HashMap<>();
 
     public int getId() {
         return id;
@@ -20,5 +24,9 @@ public class TSQueryMatch {
 
     public TSQueryCapture[] getCaptures() {
         return captures;
+    }
+
+    public Map<String, String> getMetadata() {
+        return metadata;
     }
 }
